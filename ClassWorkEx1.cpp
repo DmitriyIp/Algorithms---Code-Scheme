01001bfa75853338b23a4975bac419aecaee7000
DmitriyIp <91135391+DmitriyIp@users.noreply.github.com>
2021-11-09 13:40:52 +0500

    Add files via upload

---
+++ ClassWorkEx1.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a, b, c;
+
+	cout << "Введите три любых числа: " << endl;
+	cin >> a >> b >> c;
+	cout << "Выполняется сравнение" << endl;
+	if (a > b and a > c) {
+		cout << "Наибольшее число: " << a;
+	}
+	if (b > a and b > c) {
+		cout << "Наибольшее число: " << b;
+	}
+	if (c > a and c > b) {
+		cout << "Наибольшее число: " << c;
+	}
+	if (a == b and a == c) {
+		cout << "Числа равны, наибольшего нет";
+	}
+
+
+
+}
