25300c0a526d662a6bc62558b23bad17fa7beeeb
DmitriyIp <91135391+DmitriyIp@users.noreply.github.com>
2021-11-09 14:35:15 +0500

    Add files via upload

---
+++ ClassWorkEx2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	
+	float a, b, c;
+
+	cout << "Введите число, которое будете делить: ";
+	cin >> a;
+	cout << "Введите число, на которое будете делить: ";
+	cin >> b;
+	if (b == 0) {
+		cout << "На ноль делить нельзя";
+	}
+	else {
+		c = a / b;
+		cout << c;
+	}
+}
