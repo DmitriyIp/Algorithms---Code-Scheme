784b8e490e9c83a85dfe66194c86b9fcbdb158e4
DmitriyIp <91135391+DmitriyIp@users.noreply.github.com>
2021-11-09 14:35:45 +0500

    Add files via upload

---
+++ ClassWorkEx3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float a;
+	cout << "Введите число: ";
+	cin >> a;
+	if (a < 5) {
+		a *= 5;
+	}
+	if (a > 5 and a < 7) {
+		a /= 10;
+	}
+	if (a > 7) {
+		a += 3;
+	}
+	cout << a;
+	return 0;
+}
+
