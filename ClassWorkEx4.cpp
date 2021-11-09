ffd6868b88103ef65dfde7d74ee71559dcf14e78
DmitriyIp <91135391+DmitriyIp@users.noreply.github.com>
2021-11-09 14:38:44 +0500

    Add files via upload

---
+++ ClassWorkEx4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, i;
+	int sum = 0;
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите число, до которого будет происходить суммирование: ";
+	cin >> a;
+	if (a <= 0) {
+		cout << "Введено не натуральное число!";
+	}
+	else {
+		for (i = 1; i <= a; i++) {
+			sum += i;
+		}
+		cout << sum;
+		return 0;
+	}
+}
+
